5b9ea20e589e8c3cd0bc5a48f957c3b001654470
Nigora97 <nigoraaaaa97@gmail.com>
2025-01-09 00:15:21 +0500

    чертов маркуе

--- index.js
+++ index.js
@@ -35,3 +35,41 @@ const navMenu = document.querySelector("header nav");
 burgerMenu.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
+
+
+// Получаем элементы
+const moreAboutMeButton = document.querySelector('.more-about-me');
+const newPage = document.getElementById('new-page');
+const backToMainButton = document.getElementById('back-to-main');
+const mainContent = document.querySelector('.estetikball');
+
+// Показ новой "страницы"
+moreAboutMeButton.addEventListener('click', (e) => {
+  e.preventDefault(); // Отключаем переход по ссылке
+  mainContent.style.display = 'none'; // Скрываем основной контент
+  newPage.style.display = 'block'; // Показываем новую "страницу"
+});
+
+// Возврат к основному контенту
+backToMainButton.addEventListener('click', () => {
+  newPage.style.display = 'none'; // Скрываем новую "страницу"
+  mainContent.style.display = 'flex'; // Показываем основной контент
+});
+
+
+function openModal() {
+    document.getElementById("projectsModal").style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById("projectsModal").style.display = "none";
+  }
+  
+  // Закрытие модального окна при клике за пределами окна
+  window.onclick = function(event) {
+    if (event.target == document.getElementById("projectsModal")) {
+      closeModal();
+    }
+  }
+  
+  
